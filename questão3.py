8a6254dc9c8247779284e3b3fb028cf3730be07b
Dilberto <dilbertoms@gmail.com>
2023-03-20 23:08:32 -0300

    Initial commit

---
+++ questão3.py
@@ -0,0 +1,8 @@
+#Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles
+
+n1=1000
+n2=2000
+if n1>n2:
+    print(n1)
+else:
+    print(n2)
---
+++ questão4.py
@@ -0,0 +1,9 @@
+#Ler dois valores (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente.
+
+
+n1=1000
+n2=2000
+if n1>n2:
+    print(n1,n2)
+else:
+    print(n2,n1)
